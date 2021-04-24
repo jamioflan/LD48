83824aa18c9e35318867d8e1a7450ecf3865f8c9
jamioflan <jamioflan@hotmail.co.uk>
2021-04-24 16:24:54 +0100

    Enemyspawne

--- Assets/Scripts/Gen/LevelAssets.cs
+++ Assets/Scripts/Gen/LevelAssets.cs
@@ -10,4 +10,18 @@ public class LevelAssets : ScriptableObject
 	public float caveness = 0.5f;
 	public int roomBudget = 10;
 	public float hallwayLength = 1.5f;
+
+	public List<SpawnData> regularSpawns = new List<SpawnData>();
+	public int numToSpawn = 8;
+	public bool spawnBoss = false;
+	public SpawnData bossData = null;
+	
+	[System.Serializable]
+	public class SpawnData
+	{
+		public Transform prefab;
+		public float spawnWeight = 1.0f;
+		public int minNumPerGroup = 1;
+		public int maxNumPerGroup = 3;
+	}
 }
--- Assets/Scripts/Gen/LevelGenerator.cs
+++ Assets/Scripts/Gen/LevelGenerator.cs
@@ -11,6 +11,7 @@ public class LevelGenerator : MonoBehaviour
 	public int currentLevel = 0;
 	public Transform[,] walls = null;
 	public Transform floor = null;
+	public List<Transform> spawnedEnemies = new List<Transform>();
 
 	private const int WALL_TYPE_EMPTY = 0;
 	private const int WALL_TYPE_ROCK = 1;
@@ -43,8 +44,8 @@ public class LevelGenerator : MonoBehaviour
 				int dX = i - levelSize / 2;
 				int dZ = k - levelSize / 2;
 
-				float radius = Mathf.Sqrt(dX * dX + dZ * dZ) / levelSize + Mathf.PerlinNoise(i * 0.0913f, k * 0.07498f);
-				if(radius > assets.caveness)
+				float caveRadius = Mathf.Sqrt(dX * dX + dZ * dZ) / levelSize + Mathf.PerlinNoise(i * 0.0913f, k * 0.07498f);
+				if(caveRadius > assets.caveness)
 				{
 					wallTypes[i, k] = WALL_TYPE_ROCK;
 				}
@@ -100,6 +101,72 @@ public class LevelGenerator : MonoBehaviour
 		}
 
 		floor = Instantiate(assets.floorPrefab, transform);
+
+		// Place player spawn pos - start at outer edge and work inwards
+		Vector2Int spawnPoint = Vector2Int.zero;
+		float radius = levelSize / 2f;
+		while(spawnPoint == Vector2Int.zero && radius >= 0.0f)
+		{
+			float angle = Random.Range(0, Mathf.PI * 2f);
+			Vector2 pos = new Vector2(radius * Mathf.Cos(angle), radius * Mathf.Sin(angle));
+			Vector2Int gridPos = new Vector2Int(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y));
+			if(gridPos.x > 0 && gridPos.x < levelSize - 1 && gridPos.y > 0 && gridPos.y < levelSize - 1)
+			{
+				if (wallTypes[gridPos.x, gridPos.y] == WALL_TYPE_EMPTY)
+					spawnPoint = gridPos;
+			}
+
+			radius -= 2.0f;
+		}
+
+		// Place enemies
+		float totalWeight = 0.0f;
+		foreach(LevelAssets.SpawnData spawn in assets.regularSpawns)
+		{
+			totalWeight += spawn.spawnWeight;
+		}
+		for(int i = 0; i < assets.numToSpawn; i++)
+		{
+			float pick = Random.Range(0.0f, totalWeight);
+			int index = 0;
+			while(pick > 0.0f)
+			{
+				if (index >= assets.regularSpawns.Count)
+					break;
+				pick -= assets.regularSpawns[index].spawnWeight;
+				index++;
+			}
+
+			if (index >= assets.regularSpawns.Count)
+				break;
+
+			Spawn(assets.regularSpawns[index], wallTypes, spawnPoint);
+		}
+	}
+
+	private void Spawn(LevelAssets.SpawnData spawn, int[,] map, Vector2Int playerSpawnPos)
+	{
+		int attempts = 100;
+		bool found = false;
+		while(!found && attempts > 0)
+		{
+			attempts--;
+
+			int x = Random.Range(0, levelSize);
+			int y = Random.Range(0, levelSize);
+
+			if (map[x, y] != WALL_TYPE_EMPTY)
+				continue;
+
+			if (Vector2Int.Distance(playerSpawnPos, new Vector2Int(x, y)) < 8f)
+				continue;
+
+			found = true;
+
+			int count = Random.Range(spawn.minNumPerGroup, spawn.maxNumPerGroup + 1);
+			for(int i = 0; i < count; i++)
+				spawnedEnemies.Add(Instantiate(spawn.prefab, new Vector3(x + 0.5f, 0.0f, y + 0.5f), Quaternion.identity, transform));
+		}
 	}
 
 	private void PlaceRoom(int x, int z, int width, int height, int[,] map)
@@ -189,5 +256,11 @@ public class LevelGenerator : MonoBehaviour
 
 		if (floor != null)
 			Destroy(floor.gameObject);
+		floor = null;
+
+		foreach (Transform t in spawnedEnemies)
+			if(t != null)
+				Destroy(t.gameObject);
+		spawnedEnemies.Clear();
 	}
 }
